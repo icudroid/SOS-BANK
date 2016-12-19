8e9d07f6deb2b7b30267274d2cbb8bd959a33b59
Dimitri KAHN <dimitri@d-kahn.net>
2016-12-19 23:43:28 +0100

    chore(security) : correct unit test issue

--- apps/modules/security/service/src/test/java/net/dkahn/starter/services/Application.java
+++ apps/modules/security/service/src/test/java/net/dkahn/starter/services/Application.java
@@ -1,13 +1,16 @@
 package net.dkahn.starter.services;
 
+import net.dkahn.starter.tools.cipher.PasswordBasedEncoderConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @SpringBootApplication
 @ComponentScan("net.dkahn.starter")
+@Import(PasswordBasedEncoderConfig.class)
 public class Application {
 
 
